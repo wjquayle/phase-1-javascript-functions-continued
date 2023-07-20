66230d6e06b02cbf840494a7b93370b9054ab853
BillQuayle <wjquayle@gmail.com>
2023-07-20 19:57:33 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+  console.log(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("watch a movie");
